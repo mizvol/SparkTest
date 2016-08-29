11410d0ce7edf1ab256ff0fc0d36e6a73e9ffec0
Volodymyr Miz <volodymyr.miz@epfl.ch>
2016-08-29 11:05:53 +0200

    Added helper object to suppress Spark logs.

---
+++ src/main/scala/Helper.scala
@@ -0,0 +1,8 @@
+package org.miz.helper
+
+package object Helper {
+  def suppressLogs(params: List[String]): Unit = {
+    import org.apache.log4j.{Level, Logger}
+    params.foreach(Logger.getLogger(_).setLevel(Level.OFF))
+  }
+}
