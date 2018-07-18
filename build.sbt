1a88ab70d355726f773f547e12eeda18c3768217
Unknown <mizvladimir@gmail.com>
2018-07-18 11:34:34 +0200

    Removed GraphFrames example

--- build.sbt
+++ build.sbt
@@ -8,7 +8,6 @@ resolvers += "Spark Packages Repo" at "http://dl.bintray.com/spark-packages/mave
 
 libraryDependencies ++= Seq(
   "org.apache.spark" % "spark-core_2.10" % "2.0.0" exclude ("org.apache.hadoop","hadoop-yarn-server-web-proxy"),
-  "graphframes" % "graphframes" % "0.2.0-spark2.0-s_2.10",
   "org.apache.spark" % "spark-sql_2.10" % "2.0.0",
   "org.apache.spark" % "spark-graphx_2.10" % "2.0.0"
 )
--- src/main/scala/SparkTest.scala
+++ src/main/scala/SparkTest.scala
@@ -1,45 +1,5 @@
 import org.apache.spark.sql.SparkSession
-import org.graphframes.GraphFrame
 import org.miz.helper.Helper._
-
-object SparkGraphFrames {
-  def main(args: Array[String]): Unit = {
-
-    suppressLogs(List("org", "akka"))
-
-    val spark = SparkSession.builder
-      .master("local")
-      .appName("Spark Graph Frames")
-      .config("spark.sql.warehouse.dir","../")
-      .getOrCreate()
-
-    val v = spark.createDataFrame(List(
-      ("a", "Alice", 34),
-      ("b", "Bob", 36),
-      ("c", "Charlie", 30)
-    )).toDF("id", "name", "age")
-
-    // Create an Edge DataFrame with "src" and "dst" columns
-    val e = spark.createDataFrame(List(
-      ("a", "b", "friend"),
-      ("b", "c", "follow"),
-      ("c", "b", "follow")
-    )).toDF("src", "dst", "relationship")
-
-    // Create a GraphFrame
-    val g = GraphFrame(v, e)
-
-    // Query: Get in-degree of each vertex.
-    g.inDegrees.show()
-
-    // Query: Count the number of "follow" connections in the graph.
-    g.edges.filter("relationship = 'follow'").count()
-
-    // Run PageRank algorithm, and show results.
-    val results = g.pageRank.resetProbability(0.01).maxIter(20).run()
-    results.vertices.select("id", "pagerank").show()
-  }
-}
 
 object SparkWordCount {
   def main(args: Array[String]): Unit = {
